4204cb42f466471703cf30194daaad258fd05bca
Klipfel Nicolas <nos_67210@hotmail.fr>
2020-05-16 09:28:34 +0200

    page carte V1

---
+++ carte/carte.js
@@ -0,0 +1,23 @@
+let pages = document.querySelectorAll('.page');
+
+function toggleOpen() {
+    pages.forEach(page => {
+        if (page.classList.contains('open') && page !== this) {
+            page.classList.remove('open');
+        }
+    })
+    this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+
+    if (e.propertyName.includes('flex')) { //flex a cause du flex grow
+        this.classList.toggle('active')
+    }
+    if (!this.classList.contains('open') && this.classList.contains('active')) {
+        this.classList.remove('active');
+    };
+}
+
+pages.forEach(page => page.addEventListener('click', toggleOpen));
+pages.forEach(page => page.addEventListener('transitionend', toggleActive));
