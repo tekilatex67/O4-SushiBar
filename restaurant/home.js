836e6a4a0eec2c57f952f8da9a81e075bec97d3e
Klipfel Nicolas <nos_67210@hotmail.fr>
2020-08-15 14:34:58 +0200

    vm et desk

--- restaurant/home.js
+++ restaurant/home.js
@@ -2,13 +2,17 @@
 let u = document.querySelector('#home h2 span.u');
 let r = document.querySelector('#home h2 span.r');
 let mid;
+//SERVICE
+let boxs = document.querySelectorAll('#services .conteneur .box');
+/* REVEAL */
+let reveals = document.querySelectorAll('.reveal,#propos img');
 //OBSERVER
 let observed = document.querySelectorAll('.observed');
 //SLIDE TO SCROLL
 let home = document.querySelector('#home');
 let engagement = document.querySelector('#engagement');
-let sRight = document.querySelector('.slideRight');
-let sLeft = document.querySelector('.slideLeft');
+let sRight = document.querySelectorAll('.slideRight');
+let sLeft = document.querySelectorAll('.slideLeft');
 // ------------- BTN TOP PAGE 
 let btnTop = document.querySelector('.topPage');
 let btnAttribut = btnTop.getBoundingClientRect();
@@ -25,6 +29,9 @@ let round = document.querySelector('.contentShark');
 let roundWidht = Math.round(round.getBoundingClientRect().width);
 round.style.height = `${roundWidht}px`;
 round.style.borderRadius = `${roundWidht}px`;
+/*SLIDEBOX*/
+let slides = document.querySelector('.slides');
+let slideBox = document.querySelector('.slidesBox');
 /* SUSHI PARALAX */
 const imgPara = document.querySelectorAll('.imgPara');
 const right = document.querySelectorAll('.right');
@@ -92,9 +99,21 @@ neon(u);
 
 setTimeout(function() { neon(r) }, 3000);
 
+// ---------------- SERVICE --------------
+
+let boxWidht;
+
+if (window.innerWidth >= 800) {
+    boxs.forEach(box => {
+        boxWidht = Math.round(box.getBoundingClientRect().width);
+        box.style.height = `${boxWidht}px`;
+    });
+}
+
 // ------- FADEIN MIRROR -------------------------
 
-const ratio = .2
+const ratio = .8;
+
 const options = {
     root: null,
     rootMargin: '0px',
@@ -104,14 +123,13 @@ const options = {
 function handleIntersect(entries, observer) {
     entries.forEach(entry => {
 
-        let element = entry.target;
-        /*Pourcentage*/
-        let cpt = 0;
-        let timer = 1;
-        let n = element.dataset.to;
-        let delta = Math.ceil((timer * 1000) / n);
-
-        if (entry.intersectionRatio > ratio) {
+            let element = entry.target;
+            /*Pourcentage*/
+            let cpt = 0;
+            let timer = 1;
+            let n = element.dataset.to;
+            let delta = Math.ceil((timer * 1000) / n);
+            console.log(element);
 
             /*Pourcentage*/
             if (element.classList.contains('number')) {
@@ -124,10 +142,11 @@ function handleIntersect(entries, observer) {
                 setTimeout(count, delta);
             } else {
                 element.classList.add('actif');
-                observer.unobserve(entry.target)
+                //observer.unobserve(entry.target);
             }
         }
-    });
+
+    );
 }
 
 let observer = new IntersectionObserver(handleIntersect, options);
@@ -160,20 +179,58 @@ btnTop.addEventListener('click', scrollToTop);
 // H3 SCALE ON SCROLL 
 
 function scaleElement(Arrays) {
+
     Arrays.forEach(element => {
 
         let titleToTop = element.getBoundingClientRect().top;
         let pixels = Math.round(((titleToTop * 100) / window.innerHeight) * -1);
 
-        if (pixels <= -25) {
-            element.style.fontSize = `${pixels + 100}px`
+        if (window.innerWidth >= 800) {
+            if (pixels <= -25) {
+                element.style.fontSize = `${pixels + 100}px`
+            } else {
+                element.style.fontSize = `${(pixels*-1)+50}px`
+            }
         } else {
-            element.style.fontSize = `${(pixels*-1)+50}px`
+            element.style.fontSize = '2rem';
         }
+
     })
 }
 scaleElement(growElements);
 
+/*SLIDEBOX*/
+
+let blocslide;
+
+function sliderSize() {
+    let satiHeight = document.querySelector('#satisfaction').getBoundingClientRect().height;
+    let slideHeight = slideBox.getBoundingClientRect().height;
+    let cpt = Math.round(satiHeight / slideHeight);
+
+    //Fonction pour remove les blocSlide
+    const removeElements = (elms) => elms.forEach(el => el.remove());
+    removeElements(document.querySelectorAll(".blocSlide"));
+
+    for (let i = 0; i <= cpt; i++) {
+        blocslide = document.createElement('div');
+        blocslide.classList.add('blocSlide');
+        blocslide.innerHTML = `<div class=" slideRight flex">
+                    <img src="image/icon1.png" alt="sushi"><img src="image/icon2.png" alt="sushi"><img src="image/icon3.png" alt="sushi"><img src="image/icon4.png" alt="sushi"><img src="image/icon5.png" alt="sushi"><img src="image/icon6.png" alt="sushi">
+                    <img src="image/icon7.png" alt="sushi"><img src="image/icon8.png" alt="sushi"><img src="image/icon9.png" alt="sushi"><img src="image/icon10.png" alt="sushi"><img src="image/icon11.png" alt="sushi"><img src="image/icon12.png" alt="sushi">
+                    <img src="image/icon13.png" alt="sushi"><img src="image/icon14.png" alt="sushi"><img src="image/icon15.png" alt="sushi"><img src="image/icon16.png" alt="sushi">
+                    </div>
+                    <div class=" slideLeft flex">
+                    <img src="image/icon1.png" alt="sushi"><img src="image/icon2.png" alt="sushi"><img src="image/icon3.png" alt="sushi"><img src="image/icon4.png" alt="sushi"><img src="image/icon5.png" alt="sushi"><img src="image/icon6.png" alt="sushi">
+                    <img src="image/icon7.png" alt="sushi"><img src="image/icon8.png" alt="sushi"><img src="image/icon9.png" alt="sushi"><img src="image/icon10.png" alt="sushi"><img src="image/icon11.png" alt="sushi"><img src="image/icon12.png" alt="sushi">
+                    <img src="image/icon13.png" alt="sushi"><img src="image/icon14.png" alt="sushi"><img src="image/icon15.png" alt="sushi"><img src="image/icon16.png" alt="sushi">
+                    </div>`;
+
+        slides.appendChild(blocslide);
+
+    }
+}
+sliderSize();
 /* ------------ EVENT LISTENER ------------ */
 
 
@@ -182,28 +239,51 @@ scaleElement(growElements);
 
 window.addEventListener('scroll', () => {
 
+    //REVEAL
+    reveals.forEach(reveal => {
+        let toTop = reveal.getBoundingClientRect().top;
+        let pourcentToTop = 100 - ((toTop * 100) / window.innerHeight);
+
+        if (pourcentToTop >= 20) {
+            reveal.classList.add('active');
+        }
+        if (pourcentToTop >= 95 || pourcentToTop < 20) {
+            reveal.classList.remove('active');
+        }
+    });
+
     //SLIDE TO SCROLL
-    let attRight = sRight.getBoundingClientRect();
-    let attLeft = sLeft.getBoundingClientRect();
-    let slidPosR;
-    let slidPosL;
+    sRight = document.querySelectorAll('.slideRight');
 
-    if (attRight.top <= window.innerHeight && attRight.top >= 0 - attRight.height) {
-        slidPosR = ((`-${attRight.top +attRight.height}` - `-${window.innerHeight}`) / 50) * 1.2;
-        sRight.style.transform = `translateX(${-slidPosR}%)`;
-    }
+    sRight.forEach(right => {
+        let attRight = right.getBoundingClientRect();
+        let slidPosR;
 
-    if (attLeft.top <= window.innerHeight && attLeft.top >= 0 - attLeft.height) {
-        slidPosL = (`-${attLeft.top + attLeft.height}` - `-${window.innerHeight}`) / 50;
-        sLeft.style.transform = `translateX(${slidPosL - 80}%)`;
-    }
+        if (attRight.top <= window.innerHeight && attRight.top >= 0 - attRight.height) {
+            slidPosR = ((`-${attRight.top +attRight.height}` - `-${window.innerHeight}`) / 50) * 1.2;
+            right.style.transform = `translateX(${-slidPosR}%)`;
+        }
+    })
 
+    sLeft = document.querySelectorAll('.slideLeft');
+
+    sLeft.forEach(left => {
+        let attLeft = left.getBoundingClientRect();
+        let slidPosL;
+
+        if (attLeft.top <= window.innerHeight && attLeft.top >= 0 - attLeft.height) {
+            slidPosL = (`-${attLeft.top + attLeft.height}` - `-${window.innerHeight}`) / 50;
+            left.style.transform = `translateX(${slidPosL - 80}%)`;
+        }
+    })
 
     //BTN TOP PAGE
     btnScroll()
 
     // H3 SCALE ON SCROLL 
-    scaleElement(growElements);
+    if (window.innerWidth >= 800) {
+        scaleElement(growElements);
+    }
 
     /* SHARK ANIME ROUND */
 
@@ -237,6 +317,19 @@ window.addEventListener('scroll', () => {
 /*RESIZE*/
 
 window.addEventListener('resize', function() {
+    /*ROUND SHARK*/
     roundWidht = Math.round(round.getBoundingClientRect().width);
     round.style.height = `${roundWidht}px`;
+
+    // H3 SCALE ON SCROLL 
+
+    scaleElement(growElements);
+
+
+    /*SLIDE TO SCROLL */
+    satiHeight = document.querySelector('#satisfaction').getBoundingClientRect().height;
+    slideHeight = slideBox.getBoundingClientRect().height;
+
+    sliderSize()
+
 })
